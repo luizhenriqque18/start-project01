07662a2e2201323beda84d10e6e22ad369561efd
Luiz Henrique Oliveira de Souza <luizhenriqque15@gmail.com>
2020-03-03 09:43:07 -0400

    Start

---
+++ src/main/java/com/example/startproject01/StartProject01Application.java
@@ -0,0 +1,13 @@
+package com.example.startproject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartProject01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/startproject01/StartProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
