d73b360c5be8664128de713f00db1fe398e6fc03
Luiz Henrique Oliveira de Souza <luizhenriqque15@gmail.com>
2020-03-03 13:42:55 -0400

    Swagger

---
+++ src/main/java/com/example/startproject01/swagger/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.startproject01.swagger;
+
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfig {
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.lojavirtual")).paths(PathSelectors.any()).build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Swagger LojaVirtual API")
+                .description("Documentação da API de acesso aos endpoints com Swagger").version("1.0").build();
+    }
+}
