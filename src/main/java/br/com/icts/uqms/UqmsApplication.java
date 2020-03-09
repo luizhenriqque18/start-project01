24bc00de6ffd13b79c8a0e0e5405e3f2a3641970
jonathas araujo <jonathas.santos@grupoicts.com.br>
2020-03-09 10:30:18 -0400

    feat: add telegram bot

--- src/main/java/br/com/icts/uqms/UqmsApplication.java
+++ src/main/java/br/com/icts/uqms/UqmsApplication.java
@@ -2,12 +2,18 @@ package br.com.icts.uqms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import br.com.icts.uqms.config.TelegramBot;
 
 @SpringBootApplication
 public class UqmsApplication {
 
 	public static void main(String[] args) {
+		ApiContextInitializer.init();
 		SpringApplication.run(UqmsApplication.class, args);
+		
+		TelegramBot bot = new TelegramBot();
+		bot.sendMessage("I am a bot!");
 	}
 
 }
---
+++ src/main/java/br/com/icts/uqms/config/TelegramBot.java
@@ -0,0 +1,54 @@
+package br.com.icts.uqms.config;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    private SendMessage response = new SendMessage();
+    private Long chatId = new Long("-1001437064714");
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage()) {
+			Message message = update.getMessage();
+			SendMessage response = new SendMessage();
+			Long chatId = message.getChatId();
+			response.setChatId(chatId);
+			String text = message.getText();
+			response.setText(text);
+			try {
+				execute(response);
+			} catch (TelegramApiException e) {
+			}
+		}
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "sfhie_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1091844225:AAG0bkGyegpz-j8RihTYAVHDcFnEFX2dPcM";
+    }
+
+    public void sendMessage(String message) {
+        this.response.setChatId(this.chatId);
+        this.response.setText(message);
+        try {
+            execute(this.response);
+        } catch (TelegramApiException e) {
+        }
+    }
+
+    @PostConstruct
+    public void start() {}
+}
